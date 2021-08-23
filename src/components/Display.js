5cfdfde73db9de139a08ab86791607473f81a7ca
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-08-23 15:54:24 +0200

    Add input field for display

---
+++ src/components/Display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Display({ calculation }) {
+  return (
+    <>
+      <input id="result" value={calculation || '0'} />
+    </>
+  );
+}
+
+export default Display;
