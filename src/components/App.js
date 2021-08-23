9aefc8761dacb5510aa9b252f6c386e0501e40c1
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-08-23 20:11:44 +0200

    Add and set-up calculate and operate modules

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/calculate';
 
 function App() {
   return (
     <>
       <h1>Calculator App</h1>
-      <Display />
+      <Display calculate={calculate} />
       <ButtonPanel />
     </>
   );
