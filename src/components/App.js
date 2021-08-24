1102fa1882d94ed802d16d1038b019703fd91c08
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-08-24 09:35:45 +0200

    Add css to center calculator

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
+import '../styles/App.css';
 
 function App() {
   return (
