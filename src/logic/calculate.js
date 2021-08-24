d85c624cb224f7579a9ed0bc2fa0331945eda8bc
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-08-24 08:31:58 +0200

    Add data object and first if statement in calculate funtion

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,5 +1,10 @@
-const Calculate = () => {
+const Calculate = (data, buttonName) => {
+  let { total, next, operation} = data;
+if (buttonName == '+/-'){
+    total *= -1;
+    next *= -1;
+   }
 
-}
+};
 
 export default Calculate;
