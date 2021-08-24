ab5992c38cfd7bab703137544ca2bd70574b7322
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-08-24 08:55:24 +0200

    Add Ac, =,. and +/- operationsin calculate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,10 +1,30 @@
+import Operate from './operate';
+
 const Calculate = (data, buttonName) => {
-  let { total, next, operation} = data;
-if (buttonName == '+/-'){
+  let { total, next, operation } = data;
+  if (buttonName == '+/-') {
     total *= -1;
     next *= -1;
-   }
-
+  }
+  if (buttonName === '=') {
+    total = Operate(total, next, operation);
+  }
+  if (buttonName === 'AC') {
+    total = null;
+    next = null;
+    operation = null;
+  }
+  if (buttonName === '.') {
+    if (!total) {
+      total = '0.';
+    } else if (total && operation) {
+      total += '.';
+    } else if (total && next && operation) {
+      next += '.';
+    } else if (total && operation && !next) {
+      next = '0.';
+    }
+  }
 };
 
 export default Calculate;
