37ad2b4fd439f688da5b71e12810ca2c05c1b391
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-08-24 11:06:42 +0200

    Add handleClick to Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ name }) {
+function Button({ name, handleClick }) {
   return (
     <>
-      <button type="button">{name}</button>
+      <button type="button" onClick={handleClick}>{name}</button>
     </>
   );
 }
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default Button;
