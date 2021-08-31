c1fd09759b1eebb7a7fa86dd9341151dec251980
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-08-31 14:05:22 +0200

    Fix linter errors

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ name, handleClick, wide, color }) {
+function Button({
+  name, handleClick, wide, color,
+}) {
   return (
     <>
-      <button type="button" name={name} onClick={handleClick} 
-      style={{
-        background: color || '#f5913e',
-          width: wide ? '50%' : '25%',}}
-          >{name}</button>
+      <button
+        type="button"
+        name={name}
+        onClick={handleClick}
+        style={{
+          background: color || '#f5913e',
+          width: wide ? '50%' : '25%',
+        }}
+      >
+        {name}
+      </button>
     </>
   );
 }
@@ -16,6 +24,13 @@ function Button({ name, handleClick, wide, color }) {
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   handleClick: PropTypes.func.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.string,
+
+};
+Button.defaultProps = {
+  wide: '25%',
+  color: 'color',
 };
 
 export default Button;
